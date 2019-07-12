91d8adf291807e0df9d8bfbb350144fa75e7cc12
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-12 15:42:45 +0300

    Add email validation to LoginRoute

--- lib/src/routes/login_route.dart
+++ lib/src/routes/login_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/src/utils/constants.dart';
+import 'package:flutter_app/src/utils/validators.dart';
 
 import 'package:flutter_app/src/widgets/auth_form_field.dart';
 
@@ -11,6 +12,7 @@ class LoginRoute extends StatefulWidget {
 class _LoginRouteState extends State<LoginRoute> {
   var email = '';
   var password = '';
+  final _formKey = GlobalKey<FormState>();
 
   onChangeEmail(String value) {
     email = value;
@@ -20,47 +22,56 @@ class _LoginRouteState extends State<LoginRoute> {
     password = value;
   }
 
-  toRegistrationScreen() => Navigator.of(context).pushNamed(RouteNames.register);
+  toRegistrationScreen() =>
+      Navigator.of(context).pushNamed(RouteNames.register);
 
   @override
   Widget build(BuildContext context) {
-    _onPress() => print('email - $email, password - $password');
+    _onPress() {
+      if (_formKey.currentState.validate()) {
+        print('email - $email, password - $password');
+      }
+    }
 
     return Scaffold(
       appBar: AppBar(title: Text('Login')),
       body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.network(
-                "https://cdn.pixabay.com/photo/2019/07/06/03/30/sand-dune-4319681_960_720.jpg",
-                fit: BoxFit.contain,
-              ),
-              AuthFormField(
-                label: "Email",
-                onChangeText: onChangeEmail,
-              ),
-              AuthFormField(
-                label: "Password",
-                onChangeText: onChangePassword,
-                isObscureText: true,
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 80),
-                child: RaisedButton(
-                  onPressed: _onPress,
-                  elevation: 3,
-                  color: Theme.of(context).accentColor,
-                  child: Text('Submit'),
+        child: Form(
+          key: _formKey,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.network(
+                  "https://cdn.pixabay.com/photo/2019/07/06/03/30/sand-dune-4319681_960_720.jpg",
+                  fit: BoxFit.contain,
                 ),
-              ),
-              GestureDetector(
-                onTap: toRegistrationScreen,
-                child: Text("To registration screen"),
-              )
-            ],
+                AuthFormField(
+                  label: "Email",
+                  onChangeText: onChangeEmail,
+                  validator: Validator.email,
+                ),
+                AuthFormField(
+                  label: "Password",
+                  onChangeText: onChangePassword,
+                  isObscureText: true,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 80),
+                  child: RaisedButton(
+                    onPressed: _onPress,
+                    elevation: 3,
+                    color: Theme.of(context).accentColor,
+                    child: Text('Submit'),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: toRegistrationScreen,
+                  child: Text("To registration screen"),
+                )
+              ],
+            ),
           ),
         ),
       ),
