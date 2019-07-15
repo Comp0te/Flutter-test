0252980ace68141da3dc2060ddf0a4a4e548ff72
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-15 17:27:15 +0300

    Refactor Validators

--- lib/src/utils/validators.dart
+++ lib/src/utils/validators.dart
@@ -1,14 +1,35 @@
-var emailReg =
-    r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$";
-
 typedef ValidatorFn<T> = T Function(T value);
 
-abstract class Validator {
-  static String email(String value) {
-    if (value.isEmpty) {
+abstract class Validators {
+  static final RegExp _emailRegExp = RegExp(
+    r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$',
+  );
+
+  static final RegExp _passwordRegExp = RegExp(
+    r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$',
+  );
+
+  static bool isEmpty(String value) => value.isEmpty;
+  static bool isEmailValid(String email) => _emailRegExp.hasMatch(email);
+  static bool isPasswordValid(String password) =>
+      _passwordRegExp.hasMatch(password);
+
+  static String email(String email) {
+    if (isEmpty(email)) {
       return 'Email is requared';
-    } else if (!RegExp(emailReg).hasMatch(value)) {
+    } else if (!isEmailValid(email)) {
       return 'Enter valid email';
+    }
+
+    return null;
+  }
+
+  static String password(String password) {
+    if (isEmpty(password)) {
+      return 'Password is requared';
+    } else if (isPasswordValid(password)) {
+      return 'Password must contain 8 characters, at least'
+          'one capital letter and 1 digit';
     }
 
     return null;
