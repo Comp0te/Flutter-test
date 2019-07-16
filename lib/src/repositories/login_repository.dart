0f0ad05968aa8da0049272a1dc086b5da48664dc
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-16 22:57:28 +0300

    Rework LoginRepository login param interface

--- lib/src/repositories/login_repository.dart
+++ lib/src/repositories/login_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter_app/src/blocks/blocks.dart';
 import 'package:meta/meta.dart';
 
 import 'package:flutter_app/src/data_providers/auth_api.dart';
@@ -11,7 +12,7 @@ class LoginRepository {
   LoginRepository({@required this.authApiProvider})
       : assert(authApiProvider != null);
 
-  Future<LoginResponseModel> login(LoginInput data) async {
+  Future<LoginResponseModel> login(Submitted data) async {
     return await authApiProvider.login(data);
   }
 }
