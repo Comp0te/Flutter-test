f704fe76bd9eda9bb552baa0213b791be4f3552b
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-16 22:57:59 +0300

    Add interceptor to authApiProvider

--- lib/src/data_providers/auth_api.dart
+++ lib/src/data_providers/auth_api.dart
@@ -1,7 +1,9 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_app/src/blocks/blocks.dart';
 
 import 'package:flutter_app/src/utils/constants.dart';
 import 'package:flutter_app/src/models/model.dart';
+import 'package:meta/meta.dart';
 
 class AuthApiProvider {
   static BaseOptions options = BaseOptions(
@@ -10,18 +12,51 @@ class AuthApiProvider {
     receiveTimeout: 3000,
   );
 
-  final Dio dio = Dio(options);
+  static InterceptorsWrapper logger = InterceptorsWrapper(
+      onRequest: (RequestOptions options) {
+        // Do something before request is sent
+        return options; //continue
+        // If you want to resolve the request with some custom data，
+        // you can return a `Response` object or return `dio.resolve(data)`.
+        // If you want to reject the request with a error message,
+        // you can return a `DioError` object or return `dio.reject(errMsg)`
+      },
+      onResponse: (Response response) {
+        // Do something with response data
+        return response; // continue
+      },
+      onError: (DioError e) {
+        // Do something with response error
+        return e; //continue
+      }
+  );
+
+  Dio dio;
+
+  AuthApiProvider({@required Dio dio}) {
+    this.dio = dio;
+    this.dio.interceptors.add(logger);
+  }
+//      : assert(dio != null),
+//        dio.interceptors.add(logger);
+
+//  interceptors.add(element);
+
+  serInterceptors() {
+    dio.interceptors.add(logger);
+  }
 
   setHeaders(Map<String, String> headers) {
     dio.options.headers = headers;
   }
 
-  Future<LoginResponseModel> login(LoginInput data) async {
+  Future<LoginResponseModel> login(Submitted data) async {
     Response<LoginResponseModel> response = await dio.post<LoginResponseModel>(
       Url.login,
       data: data,
     );
 
+    print('response ------------------- $response');
     return response.data;
   }
 }
