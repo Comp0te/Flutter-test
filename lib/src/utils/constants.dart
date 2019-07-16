4a1c403dbc58268b673038415de5c3d78990d089
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-16 10:15:28 +0300

    Add Url constant

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -2,3 +2,8 @@ abstract class RouteNames {
   static const login = '/';
   static const register = '/register';
 }
+
+abstract class Url {
+  static final base = 'http://light-it-04.tk/api/';
+  static final login = 'login/';
+}
