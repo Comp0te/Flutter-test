4f6362c432f82f53f9a50a7b7b58df5c9d79844d
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-17 11:49:20 +0300

    Disable debug print transition

--- lib/src/blocks/block_delegate.dart
+++ lib/src/blocks/block_delegate.dart
@@ -16,6 +16,6 @@ class SimpleBlocDelegate extends BlocDelegate {
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    print(transition);
+//    print(transition);
   }
 }
