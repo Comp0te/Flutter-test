dbbf0004fedf297a1239acd51c6bb5a84d91c695
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-17 11:47:01 +0300

    Rework auth_api - inject dio instance and configure them

--- lib/src/data_providers/auth_api.dart
+++ lib/src/data_providers/auth_api.dart
@@ -1,53 +1,26 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_app/src/blocks/blocks.dart';
 
 import 'package:flutter_app/src/utils/constants.dart';
 import 'package:flutter_app/src/models/model.dart';
 import 'package:meta/meta.dart';
 
 class AuthApiProvider {
-  static BaseOptions options = BaseOptions(
-    baseUrl: Url.base,
-    connectTimeout: 5000,
-    receiveTimeout: 3000,
-  );
+  final Dio _dio;
 
-  static InterceptorsWrapper logger = InterceptorsWrapper(
-      onRequest: (RequestOptions options) {
-        // Do something before request is sent
-        return options; //continue
-        // If you want to resolve the request with some custom data，
-        // you can return a `Response` object or return `dio.resolve(data)`.
-        // If you want to reject the request with a error message,
-        // you can return a `DioError` object or return `dio.reject(errMsg)`
-      },
-      onResponse: (Response response) {
-        // Do something with response data
-        return response; // continue
-      },
-      onError: (DioError e) {
-        // Do something with response error
-        return e; //continue
-      }
-  );
+  AuthApiProvider({@required Dio dio}) : _dio = dio {
+    _dio.options.baseUrl = Url.base;
+    _dio.options.connectTimeout = 5000;
+    _dio.options.receiveTimeout = 3000;
+    _dio.options.responseType = ResponseType.json;
 
-  Dio dio;
-
-  AuthApiProvider({@required Dio dio}) {
-    this.dio = dio;
-    this.dio.interceptors.add(logger);
-  }
-//      : assert(dio != null),
-//        dio.interceptors.add(logger);
-
-//  interceptors.add(element);
-
-  serInterceptors() {
-    dio.interceptors.add(logger);
+    _dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
   }
 
-  setHeaders(Map<String, String> headers) {
-    dio.options.headers = headers;
+  addHeaders(Iterable<MapEntry<String, String>> headers) {
+    _dio.options.headers.addEntries(headers);
   }
 
   Future<LoginResponseModel> login(Submitted data) async {
