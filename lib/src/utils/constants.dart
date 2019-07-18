19423370c657422d5dfefced3f4ce7d34fc9351a
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-18 11:34:28 +0300

    Add new routeName constants

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -1,9 +1,14 @@
-abstract class RouteNames {
+abstract class AuthRouteNames {
   static const login = '/';
   static const register = '/register';
+}
+
+abstract class MainRouteNames {
+  static const home = '/';
 }
 
 abstract class Url {
   static final base = 'http://light-it-04.tk/api/';
   static final login = 'login/';
+  static final register = 'registration/';
 }
