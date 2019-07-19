6b5add9b49e04cfcfc419e83fd9b5382ddcb9777
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-19 16:43:07 +0300

    Refactor StateHelper

--- lib/src/utils/state_helper.dart
+++ lib/src/utils/state_helper.dart
@@ -1,27 +1,28 @@
 import 'package:meta/meta.dart';
 
 abstract class StateHelper {
-  static Map<String, T> updateEntities<T>({
-    @required Map<String, T> stateEntities,
-    @required List<T> eventEntitiesList,
+  static Map<String, V> updateEntities<V>({
+    @required Map<String, V> stateEntities,
+    @required List<V> eventEntitiesList,
   }) {
+
     if (eventEntitiesList == null) {
       return stateEntities;
     }
 
-    Map<String, T> eventEntitiesMap = Map.fromIterable(
+    Map<String, V> eventEntitiesMap = Map.fromIterable(
       eventEntitiesList,
       key: (entity) => entity.id.toString(),
       value: (entity) => entity,
     );
-    Map<String, T> newEntities = Map.from(stateEntities)
+    Map<String, V> newEntities = Map.from(stateEntities)
       ..addAll(eventEntitiesMap);
 
     return newEntities;
   }
 
-  static Map<String, T> deleteEntities<T>({
-    @required Map<String, T> stateEntities,
+  static Map<String, V> deleteEntities<V>({
+    @required Map<String, V> stateEntities,
     @required List<String> entitiesIds,
   }) {
     if (entitiesIds == null) {
