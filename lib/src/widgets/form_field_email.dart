bd3d5d5aa7c067c80e1ee99533547529862cff2e
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-21 14:02:58 +0300

    Add validatorsList field to FormFiledEmail and FormFieldPassword

--- lib/src/widgets/form_field_email.dart
+++ lib/src/widgets/form_field_email.dart
@@ -4,17 +4,19 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 class FormFieldEmail extends StatelessWidget {
   final TextEditingController controller;
   final String label;
+  final List<FormFieldValidator> validatorsList;
 
   const FormFieldEmail({
     Key key,
     @required this.controller,
     this.label = "Email",
+    this.validatorsList = const [],
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      constraints: BoxConstraints(maxHeight: 50),
+      constraints: BoxConstraints(maxHeight: 75),
       child: FormBuilderTextField(
         controller: controller,
         attribute: label,
@@ -27,6 +29,7 @@ class FormFieldEmail extends StatelessWidget {
         validators: [
           FormBuilderValidators.required(),
           FormBuilderValidators.email(),
+          ...validatorsList,
         ],
       ),
     );
--- lib/src/widgets/form_field_password.dart
+++ lib/src/widgets/form_field_password.dart
@@ -4,17 +4,19 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 class FormFieldPassword extends StatelessWidget {
   final TextEditingController controller;
   final String label;
+  final List<FormFieldValidator> validatorsList;
 
   const FormFieldPassword({
     Key key,
     @required this.controller,
     this.label = "Password",
+    this.validatorsList = const [],
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      constraints: BoxConstraints(maxHeight: 50),
+      constraints: BoxConstraints(maxHeight: 75),
       child: FormBuilderTextField(
         controller: controller,
         attribute: label,
@@ -30,6 +32,7 @@ class FormFieldPassword extends StatelessWidget {
             8,
             errorText: "Min 8 characters",
           ),
+          ...validatorsList,
         ],
       ),
     );
