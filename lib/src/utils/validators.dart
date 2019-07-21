7bbfb4422ace7a3f61623e6f496b744b61379b68
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-21 14:01:58 +0300

    Rework Validators utils

--- lib/src/utils/validators.dart
+++ lib/src/utils/validators.dart
@@ -1,37 +1,15 @@
-typedef ValidatorFn<T> = T Function(T value);
+import 'package:flutter/material.dart';
 
 abstract class Validators {
-  static final RegExp _emailRegExp = RegExp(
-    r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$',
-  );
+  static FormFieldValidator makeConfirmPasswordValidator({
+    @required TextEditingController passwordController,
+  }) {
+    return (dynamic val) {
+      if (passwordController.text != val) {
+        return "Password don't match";
+      }
 
-  static final RegExp _passwordRegExp = RegExp(
-    r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$',
-  );
-
-  static bool isEmpty(String value) => value.isEmpty;
-  static bool isEmailValid(String email) => _emailRegExp.hasMatch(email);
-  static bool isPasswordValid(String password) =>
-      _passwordRegExp.hasMatch(password);
-
-  static String email(String email) {
-    if (isEmpty(email)) {
-      return 'Email is requared';
-    } else if (!isEmailValid(email)) {
-      return 'Enter valid email';
-    }
-
-    return null;
-  }
-
-  static String password(String password) {
-    if (isEmpty(password)) {
-      return 'Password is requared';
-    } else if (isPasswordValid(password)) {
-      return 'Password must contain 8 characters, at least'
-          'one capital letter and 1 digit';
-    }
-
-    return null;
+      return null;
+    };
   }
 }
