ff804ce95ce61ffd443e594ce6850280b44d150b
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-21 23:54:57 +0300

    Add animations to LoginScreen

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -16,11 +16,99 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen>
+    with TickerProviderStateMixin {
   final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final FormValidationBloc _formValidationBloc = FormValidationBloc();
+  AnimationController _animationController;
+  Animation<double> width;
+  Animation<double> opacity;
+  Animation<EdgeInsets> marginEmail;
+  Animation<EdgeInsets> marginPassword;
+  Animation<Color> color;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    )..forward();
+
+    opacity = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(
+          0.0,
+          0.500,
+          curve: Curves.linear,
+        ),
+      ),
+    );
+    width = Tween<double>(
+      begin: 50.0,
+      end: 150.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(
+          0.125,
+          0.625,
+          curve: Curves.ease,
+        ),
+      ),
+    );
+    marginEmail = Tween<EdgeInsets>(
+      begin: EdgeInsets.only(bottom: 20),
+      end: EdgeInsets.only(bottom: 0),
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(
+          0.0,
+          0.50,
+          curve: Curves.elasticIn,
+        ),
+      ),
+    );
+    marginPassword = Tween<EdgeInsets>(
+      begin: EdgeInsets.only(bottom: 40),
+      end: EdgeInsets.only(bottom: 0),
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(
+          0.500,
+          1.00,
+          curve: Curves.bounceOut,
+        ),
+      ),
+    );
+    color = ColorTween(
+      begin: Colors.red,
+      end: Colors.blue,
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(
+          0.25,
+          1.00,
+          curve: Curves.bounceOut,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,59 +155,75 @@ class _LoginScreenState extends State<LoginScreen> {
             _loginBloc.dispatch(LoginRequestInit());
           }
         },
-        child: Center(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                BlocBuilder(
-                  bloc: _formValidationBloc,
-                  builder: (
-                    BuildContext context,
-                    FormValidationState formValidationState,
-                  ) {
-                    return FormBuilder(
-                      key: _fbKey,
-                      autovalidate: formValidationState.isFormAutoValidate,
-                      child: Column(
-                        children: <Widget>[
-                          FormFieldEmail(
-                            controller: _emailController,
+        child: AnimatedBuilder(
+          animation: _animationController,
+          builder: (BuildContext context, Widget child) {
+            return Center(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    BlocBuilder(
+                      bloc: _formValidationBloc,
+                      builder: (
+                        BuildContext context,
+                        FormValidationState formValidationState,
+                      ) {
+                        return FormBuilder(
+                          key: _fbKey,
+                          autovalidate: formValidationState.isFormAutoValidate,
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                margin: marginEmail.value,
+                                child: FormFieldEmail(
+                                  controller: _emailController,
+                                ),
+                              ),
+                              Container(
+                                margin: marginPassword.value,
+                                child: FormFieldPassword(
+                                  controller: _passwordController,
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(bottom: 40, top: 20),
+                                child: BlocBuilder(
+                                  bloc: _loginBloc,
+                                  builder: (
+                                    BuildContext context,
+                                    LoginState loginState,
+                                  ) {
+                                    return Opacity(
+                                      opacity: opacity.value,
+                                      child: SubmitButton(
+                                        isLoading: loginState.isLoading,
+                                        title: 'Submit',
+                                        color: color.value,
+                                        onPress: _getOnPressSubmit(
+                                          formValidationState,
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                              HeroRegister(
+                                width: width.value,
+                                onTap: _toRegistrationScreen,
+                              ),
+                            ],
                           ),
-                          FormFieldPassword(
-                            controller: _passwordController,
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(bottom: 40, top: 20),
-                            child: BlocBuilder(
-                              bloc: _loginBloc,
-                              builder: (
-                                BuildContext context,
-                                LoginState loginState,
-                              ) {
-                                return SubmitButton(
-                                  isLoading: loginState.isLoading,
-                                  title: 'Submit',
-                                  onPress: _getOnPressSubmit(
-                                    formValidationState,
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                          HeroRegister(
-                            width: 150,
-                            onTap: _toRegistrationScreen,
-                          ),
-                        ],
-                      ),
-                    );
-                  },
+                        );
+                      },
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
