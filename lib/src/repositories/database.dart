2fcaab35333bb516215a0267908bc12e70de3344
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-25 10:33:07 +0300

    Add getNormalizedPosters to database repository

--- lib/src/repositories/database.dart
+++ lib/src/repositories/database.dart
@@ -4,7 +4,6 @@ import 'package:flutter_app/src/models/model.dart';
 import 'package:flutter_app/src/data_providers/data_providers.dart';
 
 class DBRepository {
-
   final DBProvider dbProvider;
 
   DBRepository({
@@ -21,5 +20,45 @@ class DBRepository {
 
   Future insertPosterImages(List<PosterNormalized> posters) async {
     await dbProvider.insertPosterImages(posters);
+  }
+
+  Future<List<PosterNormalized>> getNormalizedPosters() async {
+    var postersTable = await dbProvider.getPosters();
+
+    List<PosterNormalized> posters =
+        postersTable.map<PosterNormalized>((posterMap) {
+      return PosterNormalized.fromJson(posterMap);
+    }).toList();
+
+    var posterImagesTable = await dbProvider.getPosterImages();
+
+    List<PosterImageDB> posterImages =
+        posterImagesTable.map<PosterImageDB>((posterImageMap) {
+      return PosterImageDB.fromJson(posterImageMap);
+    }).toList();
+
+    return posters.map((poster) {
+      var images =
+          posterImages.where((image) => image.posterId == poster.id).toList();
+
+      if (images.isNotEmpty) {
+        return PosterNormalized(
+          id: poster.id,
+          ownerId: poster.ownerId,
+          theme: poster.theme,
+          text: poster.text,
+          price: poster.price,
+          currency: poster.currency,
+          images: images,
+          contractPrice: poster.contractPrice,
+          location: poster.location,
+          category: poster.category,
+          activatedAt: poster.activatedAt,
+          isActive: poster.isActive,
+        );
+      }
+
+      return poster;
+    }).toList();
   }
 }
