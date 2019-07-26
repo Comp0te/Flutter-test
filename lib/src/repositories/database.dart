ee8f918f77d1d74ac7d3994bf2dede16bd47b337
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-26 09:09:02 +0300

    Rework getNormalizedPosters method of database repository

--- lib/src/repositories/database.dart
+++ lib/src/repositories/database.dart
@@ -25,9 +25,9 @@ class DBRepository {
   Future<List<PosterNormalized>> getNormalizedPosters() async {
     var postersTable = await dbProvider.getPosters();
 
-    List<PosterNormalized> posters =
-        postersTable.map<PosterNormalized>((posterMap) {
-      return PosterNormalized.fromJson(posterMap);
+    List<PosterNormalizedDB> posters =
+        postersTable.map<PosterNormalizedDB>((posterMap) {
+      return PosterNormalizedDB.fromJson(posterMap);
     }).toList();
 
     var posterImagesTable = await dbProvider.getPosterImages();
@@ -37,28 +37,23 @@ class DBRepository {
       return PosterImageDB.fromJson(posterImageMap);
     }).toList();
 
-    return posters.map((poster) {
+    return posters.map<PosterNormalized>((poster) {
       var images =
           posterImages.where((image) => image.posterId == poster.id).toList();
-
-      if (images.isNotEmpty) {
-        return PosterNormalized(
-          id: poster.id,
-          ownerId: poster.ownerId,
-          theme: poster.theme,
-          text: poster.text,
-          price: poster.price,
-          currency: poster.currency,
-          images: images,
-          contractPrice: poster.contractPrice,
-          location: poster.location,
-          category: poster.category,
-          activatedAt: poster.activatedAt,
-          isActive: poster.isActive,
-        );
-      }
-
-      return poster;
+      return PosterNormalized(
+        id: poster.id,
+        ownerId: poster.ownerId,
+        theme: poster.theme,
+        text: poster.text,
+        price: poster.price,
+        currency: poster.currency,
+        images: images.isNotEmpty ? images : null,
+        contractPrice: poster.contractPrice == 1 ? true : false,
+        location: poster.location,
+        category: poster.category,
+        activatedAt: poster.activatedAt,
+        isActive: poster.isActive == 1 ? true : false,
+      );
     }).toList();
   }
 }
