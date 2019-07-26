f4e3a4b682e6f51effc2ea4689968d6e57ed0dcd
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-26 08:44:56 +0300

    Add new column to posters_table

--- lib/src/database/tables/posters_table.dart
+++ lib/src/database/tables/posters_table.dart
@@ -15,11 +15,13 @@ abstract class PostersTable {
   static final colActivatedAt = 'activated_at';
   static final colIsActive = 'is_active';
 
-  static final create = '''CREATE TABLE ${PostersTable.name}(
+  static final create = '''CREATE TABLE ${name}(
         ${colPosterId} INTEGER PRIMARY KEY,
         ${colOwnerId} INTEGER,
         ${colTheme} TEXT,
+        ${colText} TEXT,
         ${colPrice} REAL,
+        ${colCurrency} REAL,
         ${colContractPrice} INTEGER,
         ${colLocation} TEXT,
         ${colCategory} TEXT,
