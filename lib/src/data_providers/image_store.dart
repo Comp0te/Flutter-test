235600287fea825a9ea1a23753c7948cd0a29e0e
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-07-30 17:24:29 +0300

    Rework image_store data_provider

--- lib/src/data_providers/image_store.dart
+++ lib/src/data_providers/image_store.dart
@@ -1,35 +1,49 @@
-import 'dart:io' as Io;
+import 'dart:io' as io;
+import 'package:meta/meta.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:image/image.dart';
 
-class ImagesStoreProvider {
+class ImageStoreProvider {
   Future<String> get _directoryPath async {
     final directory = await getApplicationDocumentsDirectory();
 
     return directory.path;
   }
 
-  String getFileName(String url) {
+  String _getFileName(String url) {
     final urlArray = url.split('/');
 
     return urlArray[urlArray.length - 1];
   }
 
-  Future<Io.File> getImage(String url) async {
-    return Io.File(join(await _directoryPath, getFileName(url)));
+  Future<io.File> getImageFile(String url) async {
+    return io.File(join(await _directoryPath, _getFileName(url)));
   }
 
-  Future<Io.File> getImageFromNetwork(String url) async {
+  Future<io.File> getImage(String url) async {
+    final imageFile = await getImageFile(url);
+    final bool isImageFileExist = await imageFile.exists();
+
+    if (isImageFileExist) {
+      return imageFile;
+    }
+
+    return null;
+  }
+
+  Future<io.File> fetchImageFromNetwork(String url) async {
     return DefaultCacheManager().getSingleFile(url);
   }
 
-  Future<Io.File> saveImage(String url) async {
-    final imageFromNetwork = await getImageFromNetwork(url);
+  Future<void> saveImage({
+    @required String url,
+  }) async {
+    final imageFile = await getImageFile(url);
+    final imageFromNetwork = await fetchImageFromNetwork(url);
     final decodedImage = decodeImage(await imageFromNetwork.readAsBytes());
-    final image = await getImage(url);
 
-    return image.writeAsBytes(encodeJpg(decodedImage));
+    await imageFile.writeAsBytes(encodeJpg(decodedImage));
   }
 }
