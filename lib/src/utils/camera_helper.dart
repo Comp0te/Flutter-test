00df2aaf7a3cc376ed67606a8adf1aa1a80d827a
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-08-01 12:49:35 +0300

    Add camera_helper

---
+++ lib/src/utils/camera_helper.dart
@@ -0,0 +1,17 @@
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+abstract class CameraHelper {
+  static IconData getCameraLensIcon(CameraLensDirection direction) {
+    switch (direction) {
+      case CameraLensDirection.back:
+        return Icons.camera_rear;
+      case CameraLensDirection.front:
+        return Icons.camera_front;
+      case CameraLensDirection.external:
+        return Icons.camera;
+    }
+    throw ArgumentError('Unknown lens direction');
+  }
+}
