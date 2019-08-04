0e1f100ac966a434bcf9b157f486d6be36a520a4
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-08-04 22:54:53 +0300

    Add camera_preview_screen

---
+++ lib/src/screens/camera_preview_screen.dart
@@ -0,0 +1,112 @@
+import 'dart:io';
+
+import 'package:back_button_interceptor/back_button_interceptor.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:flutter_app/src/blocs/blocs.dart';
+import 'package:flutter_app/src/models/model.dart';
+import 'package:flutter_app/src/utils/constants.dart';
+
+class CameraPreviewScreen extends StatefulWidget {
+  final CameraPreviewScreenArgs screenArgs;
+
+  const CameraPreviewScreen({Key key, @required this.screenArgs})
+      : assert(screenArgs != null),
+        super(key: key);
+
+  @override
+  _CameraPreviewScreenState createState() => _CameraPreviewScreenState();
+}
+
+class _CameraPreviewScreenState extends State<CameraPreviewScreen> {
+  CameraBloc _cameraBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _cameraBloc = BlocProvider.of<CameraBloc>(context);
+    BackButtonInterceptor.add(androidBackHandler);
+  }
+
+  @override
+  void dispose() {
+    BackButtonInterceptor.remove(androidBackHandler);
+    super.dispose();
+  }
+
+  bool androidBackHandler(bool stopDefaultButtonEvent) {
+    _cameraBloc.dispatch(ResetCameraFiles());
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add_photo_alternate,
+          color: Colors.white,
+          size: 40,
+        ),
+        heroTag: HeroTag.cameraFAB,
+        backgroundColor: Colors.green,
+        onPressed: () {
+          _cameraBloc.dispatch(ResetCameraFiles());
+          Navigator.of(context).pop();
+        },
+      ),
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: widget.screenArgs.photoPath != null
+                      ? Image.file(
+                          File(widget.screenArgs.photoPath),
+                          fit: BoxFit.contain,
+                        )
+                      : Text('VideoPlayer'),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 80,
+            right: 17,
+            child: Hero(
+              tag: HeroTag.cameraIconButton,
+              child: Material(
+                shadowColor: Colors.blue,
+                color: Colors.transparent,
+                child: IconButton(
+                  iconSize: 40,
+                  icon: Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                    size: 40,
+                  ),
+                  onPressed: () {
+                    _cameraBloc.dispatch(DeleteCameraFile(
+                      path: widget.screenArgs.photoPath != null
+                          ? widget.screenArgs.photoPath
+                          : widget.screenArgs.videoPath,
+                    ));
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/src/screens/screens.dart
+++ lib/src/screens/screens.dart
@@ -3,5 +3,6 @@ export './register_screen.dart';
 export './home_screen.dart';
 export './database_screen.dart';
 export './camera_screen.dart';
+export './camera_preview_screen.dart';
 
 export './animated_login_screen.dart';
