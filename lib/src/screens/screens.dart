4ac247ecb86f4e626a18d442c23f52a96278d4b8
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-08-05 13:57:10 +0300

    Move login_screen and animated_login_screen to login_screen folder

--- lib/src/screens/screens.dart
+++ lib/src/screens/screens.dart
@@ -1,8 +1,8 @@
-export './login_screen.dart';
+export 'package:flutter_app/src/screens/login_screen/login_screen.dart';
 export './register_screen.dart';
 export './home_screen.dart';
 export './database_screen.dart';
 export './camera_screen.dart';
 export './camera_preview_screen.dart';
 
-export './animated_login_screen.dart';
+export 'package:flutter_app/src/screens/login_screen/animated_login_screen.dart';
