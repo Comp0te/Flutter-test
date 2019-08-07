963f19922d2ab66ec7a5f56715adf7259fe20c45
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-08-07 17:14:05 +0300

    Rework image_store data provider to decode and write images in isolate

--- lib/src/data_providers/image_store.dart
+++ lib/src/data_providers/image_store.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
 import 'dart:io' as io;
-import 'package:meta/meta.dart';
+
+import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:image/image.dart';
+
+import 'package:flutter_app/src/models/model.dart';
 
 class ImageStoreProvider {
   Future<String> get _directoryPath async {
@@ -37,13 +41,21 @@ class ImageStoreProvider {
     return DefaultCacheManager().getSingleFile(url);
   }
 
-  Future<void> saveImage({
-    @required String url,
-  }) async {
+  Future<void> computeSaveImage(String url) async {
     final imageFile = await getImageFile(url);
     final imageFromNetwork = await fetchImageFromNetwork(url);
-    final decodedImage = decodeImage(await imageFromNetwork.readAsBytes());
 
-    await imageFile.writeAsBytes(encodeJpg(decodedImage));
+    var message = SaveImageIsolateMessage(
+      fileForSaving: imageFile,
+      fileFromNetwork: imageFromNetwork,
+    );
+
+    return compute<SaveImageIsolateMessage, void>(saveImage, message);
   }
+}
+
+Future<void> saveImage(SaveImageIsolateMessage msg) async {
+  final decodedImage = decodeImage(await msg.fileFromNetwork.readAsBytes());
+
+  await msg.fileForSaving.writeAsBytes(encodeJpg(decodedImage));
 }
