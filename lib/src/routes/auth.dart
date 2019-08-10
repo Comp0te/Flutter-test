51dd890a1c69455137bc6e059a9098f01c27cca5
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-08-10 15:30:51 +0300

    Create AuthRoutes and MainRoutes

---
+++ lib/src/routes/auth.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/src/blocs/blocs.dart';
+import 'package:flutter_app/src/repositories/repositories.dart';
+import 'package:flutter_app/src/screens/screens.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:page_transition/page_transition.dart';
+
+abstract class AuthRouteNames {
+  static const login = '/';
+  static const register = '/register';
+}
+
+abstract class AuthRoutes {
+  static SecureStorageRepository _secureStorageRepository(
+          BuildContext context) =>
+      RepositoryProvider.of<SecureStorageRepository>(context);
+
+  static AuthRepository _authRepository(BuildContext context) =>
+      RepositoryProvider.of<AuthRepository>(context);
+
+  static Route<AnimatedLoginScreen> loginRoute(BuildContext context) {
+    return PageTransition(
+      type: PageTransitionType.size,
+      alignment: Alignment.center,
+      child: BlocProvider(
+        builder: (context) {
+          return LoginBloc(
+            secureStorageRepository: _secureStorageRepository(context),
+            authRepository: _authRepository(context),
+          );
+        },
+        child: AnimatedLoginScreen(),
+      ),
+    );
+  }
+
+  static Route<RegisterScreen> registerRoute(BuildContext context) {
+    return PageTransition(
+      type: PageTransitionType.size,
+      alignment: Alignment.center,
+      child: BlocProvider(
+        builder: (context) {
+          return RegisterBloc(
+            secureStorageRepository: _secureStorageRepository(context),
+            authRepository: _authRepository(context),
+          );
+        },
+        child: RegisterScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/src/routes/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/src/blocs/blocs.dart';
+import 'package:flutter_app/src/repositories/repositories.dart';
+import 'package:flutter_app/src/screens/screens.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:page_transition/page_transition.dart';
+
+abstract class MainRouteNames {
+  static const home = '/';
+  static const database = '/database';
+  static const camera = '/camera';
+  static const cameraPreview = '/camera/preview';
+}
+
+abstract class MainRoutes {
+  static PostersRepository _postersRepository(BuildContext context) =>
+      RepositoryProvider.of<PostersRepository>(context);
+  static DBRepository _dbRepository(BuildContext context) =>
+      RepositoryProvider.of<DBRepository>(context);
+  static ImageStoreRepository _imageStoreRepository(BuildContext context) =>
+      RepositoryProvider.of<ImageStoreRepository>(context);
+  static CameraRepository _cameraRepository(BuildContext context) =>
+      RepositoryProvider.of<CameraRepository>(context);
+
+  static AppStateBloc _appStateBloc(BuildContext context) =>
+      BlocProvider.of<AppStateBloc>(context);
+
+  static Route<AnimatedLoginScreen> homeScreenRoute(BuildContext context) {
+    return PageTransition(
+      type: PageTransitionType.fade,
+      alignment: Alignment.center,
+      child: BlocProvider<PostersFetchBloc>(
+        builder: (context) {
+          return PostersFetchBloc(
+            appStateBloc: _appStateBloc(context),
+            postersRepository: _postersRepository(context),
+            dbRepository: _dbRepository(context),
+            imageStoreRepository: _imageStoreRepository(context),
+          );
+        },
+        child: HomeScreen(),
+      ),
+    );
+  }
+
+  static Route<RegisterScreen> databaseScreenRoute(BuildContext context) {
+    return PageTransition(
+      type: PageTransitionType.rightToLeft,
+      alignment: Alignment.center,
+      child: BlocProvider<DBBloc>(
+        builder: (context) {
+          return DBBloc(
+            appStateBloc: _appStateBloc(context),
+            dbRepository: _dbRepository(context),
+          )..dispatch(DBGetNormalizedPosters());
+        },
+        child: DatabaseScreen(),
+      ),
+    );
+  }
+
+  static Route<RegisterScreen> cameraScreenRoute(BuildContext context) {
+    return PageTransition(
+      type: PageTransitionType.fade,
+      alignment: Alignment.center,
+      child: BlocProvider<CameraBloc>(
+        builder: (context) {
+          return CameraBloc(
+            cameraRepository: _cameraRepository(context),
+          )..dispatch(InitCamera());
+        },
+        child: CameraScreen(),
+      ),
+    );
+  }
+
+  static Route<RegisterScreen> cameraPreviewScreenRoute(
+    BuildContext context,
+    CameraBloc cameraBloc,
+  ) {
+    return PageTransition(
+      type: PageTransitionType.leftToRight,
+      alignment: Alignment.centerLeft,
+      child: BlocProvider<CameraBloc>.value(
+        value: cameraBloc,
+        child: CameraPreviewScreen(),
+      ),
+    );
+  }
+}
