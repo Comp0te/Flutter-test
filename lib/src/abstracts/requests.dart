1467979fd08107c8706f65efc23f6d0007fc7069
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-10-26 01:00:36 +0300

    Add RequestState and RequestPaginatedState abstract classes

--- lib/src/abstracts/requests.dart
+++ lib/src/abstracts/requests.dart
@@ -1,3 +1,5 @@
+import 'package:meta/meta.dart';
+
 abstract class RequestInit {}
 
 abstract class RequestSuccess<T> {
@@ -10,4 +12,49 @@ abstract class RequestFailure {
   final Exception error;
 
   RequestFailure({this.error});
+}
+
+abstract class RequestState<T> {
+  final bool isLoading;
+  final T data;
+  final Exception error;
+
+  bool get isSuccess => data != null;
+  bool get isFailure => error != null;
+
+  RequestState({this.isLoading, this.data, this.error});
+
+  RequestState<T> copyWith({
+    @required bool isLoading,
+    T data,
+    Exception error,
+  }) {
+    return this;
+  }
+}
+
+abstract class RequestPaginatedState<T> {
+  final bool isLoadingFirstPage;
+  final bool isLoadingNextPage;
+  final T data;
+  final Exception error;
+
+  RequestPaginatedState(
+    this.isLoadingFirstPage,
+    this.isLoadingNextPage,
+    this.data,
+    this.error,
+  );
+
+  bool get isSuccess => data != null;
+  bool get isFailure => error != null;
+
+  RequestPaginatedState<T> copyWith({
+    bool isLoadingFirstPage,
+    bool isLoadingNextPage,
+    T data,
+    Exception error,
+  }) {
+    return this;
+  }
 }
