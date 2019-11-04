ecdccc595334cd653c9423c5729a4cf4734605d2
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-11-04 15:16:49 +0200

    test commit

--- lib/src/blocs/auth/auth_bloc.dart
+++ lib/src/blocs/auth/auth_bloc.dart
@@ -16,6 +16,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   })  : assert(secureStorageRepository != null),
         assert(authRepository != null);
 
+// test
   @override
   AuthState get initialState => AuthState.init();
 
