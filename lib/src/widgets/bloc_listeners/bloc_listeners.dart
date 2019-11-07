686b45742683a936d5bdc488caa0ed5bcb74f825
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-11-07 14:45:56 +0200

    Add auth_multi_login_bloc_listener

--- lib/src/widgets/bloc_listeners/bloc_listeners.dart
+++ lib/src/widgets/bloc_listeners/bloc_listeners.dart
@@ -3,3 +3,4 @@ export './poster_fetch_bloc_listener.dart';
 export './camera_bloc_listener.dart';
 export './app_bloc_listener.dart';
 export './refresh_request_bloc_listener.dart';
+export './auth_multi_login_bloc_listener.dart';
