8a91776917f9f233e312e4b1760048ee428dbc8f
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-11-11 18:12:25 +0200

    Add getLanguage and getLanguageCode functions

--- lib/src/constants/languages.dart
+++ lib/src/constants/languages.dart
@@ -2,7 +2,30 @@ import 'package:flutter/material.dart';
 
 enum SupportedLanguages { en, ru }
 
-final mapLanguagesEnumToLocale = <SupportedLanguages, Locale>{
-  SupportedLanguages.en: const Locale('en', ''),
-  SupportedLanguages.ru: const Locale('ru', ''),
+const englishLanguageCode = 'en';
+const russianLanguageCode = 'ru';
+const defaultLocale = Locale(englishLanguageCode, '');
+
+SupportedLanguages getLanguage(String languageCode) {
+  if (languageCode == russianLanguageCode) {
+    return SupportedLanguages.ru;
+  }
+
+  return SupportedLanguages.en;
+}
+
+String getLanguageCode(SupportedLanguages language) {
+  switch (language) {
+    case SupportedLanguages.en:
+      return englishLanguageCode;
+    case SupportedLanguages.ru:
+      return russianLanguageCode;
+    default:
+      return englishLanguageCode;
+  }
+}
+
+const mapLanguagesEnumToLocale = <SupportedLanguages, Locale>{
+  SupportedLanguages.en: defaultLocale,
+  SupportedLanguages.ru: Locale(russianLanguageCode, ''),
 };
