2462c187ff18de63f012aff493554282acc5655f
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-11-13 11:15:11 +0200

    Rework DatabaseScreen:
    - refactor code
    - use ThemeMixin

--- lib/src/screens/database_screen.dart
+++ lib/src/screens/database_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:flutter_app/src/constants/constants.dart';
 import 'package:flutter_app/generated/i18n.dart';
 import 'package:flutter_app/src/repositories/repositories.dart';
 import 'package:flutter_app/src/mixins/mixins.dart';
@@ -9,7 +8,7 @@ import 'package:flutter_app/src/models/model.dart';
 import 'package:flutter_app/src/blocs/blocs.dart';
 import 'package:flutter_app/src/widgets/widgets.dart';
 
-class DatabaseScreen extends StatefulWidget with OrientationMixin {
+class DatabaseScreen extends StatefulWidget with OrientationMixin, ThemeMixin {
   @override
   _DatabaseScreenState createState() => _DatabaseScreenState();
 }
@@ -23,47 +22,15 @@ class _DatabaseScreenState extends State<DatabaseScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final _imageStoreRepository =
-        RepositoryProvider.of<ImageStoreRepository>(context);
     final showImageCount = widget.isPortrait(context) ? 3 : 5;
-
-    Future<List<Widget>> _getImagesFromStore(
-      PosterNormalized poster,
-      int showImageCount,
-    ) {
-      if (poster.images != null && poster.images.isNotEmpty) {
-        return Stream.fromIterable(poster.images)
-            .take(showImageCount)
-            .map((image) => Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: SizedBox(
-                    height: 50,
-                    width: 50,
-                    child: ImageFromStore(
-                      imageStoreBloc: ImageStoreBloc(
-                        imageStoreRepository: _imageStoreRepository,
-                      ),
-                      url: image.file,
-                    ),
-                  ),
-                ))
-            .toList();
-      }
-
-      return Future.value([
-        Image.asset(
-          ImageAssets.posterPlaceholder,
-          fit: BoxFit.cover,
-          width: 50,
-          height: 50,
-        )
-      ]);
-    }
 
     return Scaffold(
       drawer: const MainDrawer(),
       appBar: AppBar(
-        title: Text(S.of(context).postersDB),
+        title: Text(
+          S.of(context).postersDB,
+          style: widget.getPrimaryTextTheme(context).headline,
+        ),
         centerTitle: true,
       ),
       body: SafeArea(
@@ -71,60 +38,97 @@ class _DatabaseScreenState extends State<DatabaseScreen> {
           builder: (context, state) {
             final posters = state.posters.values.toList();
 
-            return state.posters.isEmpty
-                ? const Spinner()
-                : ListView.separated(
-                    itemBuilder: (context, int index) => Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                          flex: 1,
-                          child: ListTile(
-                            contentPadding: const EdgeInsets.symmetric(
-                              vertical: 5,
-                              horizontal: 10,
-                            ),
-                            title: Text(
-                              'id - ${posters[index].id} - ${posters[index].text}',
-                            ),
-                            subtitle: Text(
-                              S.of(context).getPrice('${posters[index].price}'),
-                            ),
-                          ),
-                        ),
-                        FutureBuilder<List<Widget>>(
-                          future: _getImagesFromStore(
-                            posters[index],
-                            showImageCount,
-                          ),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.done) {
-                              return Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 10,
-                                ),
-                                child: Row(
-                                  children: snapshot.data,
-                                  mainAxisSize: MainAxisSize.min,
-                                ),
-                              );
-                            }
+            if (state.posters.isEmpty) return const Spinner();
 
-                            return const Spinner();
-                          },
-                        ),
-                      ],
+            return ListView.separated(
+              itemBuilder: (context, int index) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
+                      title: Text(
+                        'id - ${posters[index].id} - ${posters[index].text}',
+                        style: widget.getTextTheme(context).title,
+                      ),
+                      subtitle: Text(
+                        S.of(context).getPrice('${posters[index].price}'),
+                        style: widget.getTextTheme(context).body1,
+                      ),
                     ),
-                    itemCount: state.posters.length,
-                    separatorBuilder: (context, int index) => Divider(
-                      height: 2,
-                      color: index % 2 == 0 ? Colors.red : Colors.blue,
+                  ),
+                  FutureBuilder<List<Widget>>(
+                    future: _getImagesFromStore(
+                      context: context,
+                      poster: posters[index],
+                      showImageCount: showImageCount,
                     ),
-                  );
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                          ),
+                          child: Row(
+                            children: snapshot.data,
+                            mainAxisSize: MainAxisSize.min,
+                          ),
+                        );
+                      }
+
+                      return const Spinner();
+                    },
+                  ),
+                ],
+              ),
+              itemCount: state.posters.length,
+              separatorBuilder: (context, int index) => Divider(
+                color: widget.getTheme(context).dividerColor,
+                thickness: 2,
+              ),
+            );
           },
         ),
       ),
     );
+  }
+
+  Future<List<Widget>> _getImagesFromStore({
+    BuildContext context,
+    PosterNormalized poster,
+    int showImageCount,
+  }) {
+    final _imageStoreRepository =
+        RepositoryProvider.of<ImageStoreRepository>(context);
+
+    if (poster.images != null && poster.images.isNotEmpty) {
+      return Stream.fromIterable(poster.images)
+          .take(showImageCount)
+          .map((image) => Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: SizedBox(
+                  height: 50,
+                  width: 50,
+                  child: ImageFromStore(
+                    imageStoreBloc: ImageStoreBloc(
+                      imageStoreRepository: _imageStoreRepository,
+                    ),
+                    url: image.file,
+                  ),
+                ),
+              ))
+          .toList();
+    }
+
+    return Future.value([
+      const PosterPlaceholderImage(
+        width: 50,
+        height: 50,
+      )
+    ]);
   }
 }
