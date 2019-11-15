74f7868d87dc973bc82b9a154b7f0b894c1db58f
Dmitry Vynnyk <simbiatoff@gmail.com>
2019-11-15 12:59:39 +0200

    Rework home_screen load more posters spinner

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -42,7 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ..add(PostersFetchFirstPageRequest());
 
     loadMorePostersSubscription = _loadMorePostersSubject.stream
-        .debounceTime(Duration(milliseconds: 100))
+        .debounceTime(const Duration(milliseconds: 100))
         .listen((event) => _postersFetchBloc.add(event));
   }
 
@@ -75,66 +75,79 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final gridViewColumnCount = widget.isPortrait(context) ? 2 : 3;
 
-    return Scaffold(
-      drawer: const MainDrawer(),
-      appBar: _buildAppBar(context, gridViewColumnCount),
-      body: NotificationListener<ScrollNotification>(
-        onNotification: _handleScrollNotification,
-        child: BlocBuilder<AppStateBloc, AppState>(
-          builder: (context, appState) {
-            final postersList = appState.posters.values.toList();
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Scaffold(
+          drawer: const MainDrawer(),
+          appBar: _buildAppBar(context, gridViewColumnCount),
+          body: NotificationListener<ScrollNotification>(
+            onNotification: _handleScrollNotification,
+            child: BlocBuilder<AppStateBloc, AppState>(
+              builder: (context, appState) {
+                final postersList = appState.posters.values.toList();
 
-            if (postersList.isEmpty) return const Spinner();
+                if (postersList.isEmpty) return const Spinner();
 
-            return SafeArea(
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: RefreshRequestBlocListener<PostersFetchBloc,
-                        PostersFetchState>(
-                      onRefresh: () {
-                        BlocProvider.of<PostersFetchBloc>(context).add(
-                          PostersFetchFirstPageRequest(),
-                        );
-                      },
-                      child: GridView.builder(
-                        key: _gridViewKey,
-                        padding: EdgeInsets.symmetric(
-                          horizontal: gridViewPaddingHorizontal,
-                          vertical: gridViewPaddingVertical,
-                        ),
-                        controller: _scrollController,
-                        itemCount: postersList.length,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: gridViewColumnCount,
-                          crossAxisSpacing: gridViewCrossAxisSpacing,
-                          mainAxisSpacing: gridViewMainAxisSpacing,
-                        ),
-                        itemBuilder: (BuildContext context, int index) {
-                          final imageUrl = postersList[index]?.images == null ||
-                                  postersList[index].images.isEmpty
-                              ? null
-                              : postersList[index].images[0]?.file;
-
-                          return _buildPosterImage(imageUrl: imageUrl);
-                        },
-                      ),
+                return RefreshRequestBlocListener<PostersFetchBloc,
+                    PostersFetchState>(
+                  onRefresh: () {
+                    BlocProvider.of<PostersFetchBloc>(context).add(
+                      PostersFetchFirstPageRequest(),
+                    );
+                  },
+                  child: GridView.builder(
+                    key: _gridViewKey,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: gridViewPaddingHorizontal,
+                      vertical: gridViewPaddingVertical,
                     ),
-                  ),
-                  BlocBuilder<PostersFetchBloc, PostersFetchState>(
-                    builder: (context, state) {
-                      return state is PostersFetchNextRequestLoading
-                          ? const Spinner()
-                          : Container();
+                    controller: _scrollController,
+                    itemCount: postersList.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: gridViewColumnCount,
+                      crossAxisSpacing: gridViewCrossAxisSpacing,
+                      mainAxisSpacing: gridViewMainAxisSpacing,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      final imageUrl = postersList[index]?.images == null ||
+                              postersList[index].images.isEmpty
+                          ? null
+                          : postersList[index].images[0]?.file;
+
+                      if (index == postersList.length) {
+                        return Column(
+                          children: <Widget>[
+                            _buildPosterImage(imageUrl: imageUrl),
+                          ],
+                        );
+                      }
+
+                      return _buildPosterImage(imageUrl: imageUrl);
                     },
-                  )
-                ],
-              ),
-            );
-          },
+                  ),
+                );
+              },
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          bottom: 50,
+          left: MediaQuery.of(context).size.width / 2 - 25,
+          child: BlocBuilder<PostersFetchBloc, PostersFetchState>(
+            builder: (context, state) {
+              return DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                ),
+                child: state is PostersFetchNextRequestLoading
+                    ? const Spinner()
+                    : Container(),
+              );
+            },
+          ),
+        )
+      ],
     );
   }
 
